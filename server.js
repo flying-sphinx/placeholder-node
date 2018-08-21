bcb2971a01e8f295bba2c511bdc47b0edb6470f9
Pat Allan <pat@freelancing-gods.com>
2018-08-21 19:36:38 +1000

    Simple server to hold a process/pid spot.

---
+++ server.js
@@ -0,0 +1,13 @@
+var npid = require('npid'),
+    http = require('http');
+
+if (process.env.NODE_PID_FILE) {
+  npid.create(process.env.NODE_PID_FILE).removeOnExit();
+} else {
+  npid.create('/srv/nile/nile/shared/nile.pid').removeOnExit();
+}
+
+http.createServer(function (request, response) {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.end('OK\n');
+}).listen(9000, '127.0.0.1');
